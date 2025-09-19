1bad5e185165f6fe5d4198f88753bba31164102d
prathmesh9691 <patelprathmesh45@gmail.com>
2025-09-20 01:33:34 +0530

    Initial Commit

---
+++ InspoQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspoQuotes
+//
+//  Created by Apple on 20/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
